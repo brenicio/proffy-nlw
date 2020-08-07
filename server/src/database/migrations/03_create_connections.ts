50077ba5a81f0bf623a4c84d7d2263dde2d614a8
brenicio <brenicio_guilherme2@hotmail.com>
2020-08-06 23:43:43 -0300

    API created

--- server/src/database/migrations/03_create_connections.ts
+++ server/src/database/migrations/03_create_connections.ts
@@ -2,8 +2,7 @@ import Knex from 'knex';
 
 export async function up(knex: Knex){
     return knex.schema.createTable('connections', table => {
-        table.increments('id').primary();
-        
+        table.increments('id').primary();       
 
 
         table.integer('user_id')
@@ -14,7 +13,7 @@ export async function up(knex: Knex){
             .onDelete('CASCADE');
         
             table.timestamp('created_at')
-                .defaultTo('now()')
+                .defaultTo(knex.raw('CURRENT_TIMESTAMP'))
                 .notNullable();
     });
 }
